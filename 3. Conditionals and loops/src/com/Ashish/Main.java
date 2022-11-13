65371488f61ef849e0e62a2206cdd08b05e6f737
Ashish-khanagwal <ashishkhanagwal2001@gmail.com>
2022-11-13 23:33:16 +0530

    Ci: new folder addded

---
+++ 3. Conditionals and loops/src/com/Ashish/Main.java
@@ -0,0 +1,13 @@
+package com.Ashish;
+
+public class Main {
+    public static void main(String[] args){
+
+//        If - else syntax
+//        if(boolean expression T or F){
+//            body
+//        } else {
+//            do this
+//        }
+    }
+}
