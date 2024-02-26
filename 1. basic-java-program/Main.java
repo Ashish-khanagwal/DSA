b20d37bfc564a66e95c770ef2a7c1a4b76cf5563
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2024-02-26 20:20:49 +0530

    Restarting the playlist

--- 1. basic-java-program/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-// Public means "Main" class is accessible from anywhere.
-  public static void main(String[] args){
-    System.out.println("Hello World!");
-  }
-}
--- Assignments/3. Conditionals and Loops/src/com/Ashish/Leetcode_ques.java
+++
@@ -1,9 +0,0 @@
-package com.Ashish;
-
-import java.util.Scanner;
-
-public class Leetcode_ques {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-    }
-}
