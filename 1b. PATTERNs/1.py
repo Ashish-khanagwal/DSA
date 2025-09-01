e4ea68079b2f426ac1776d4cdb3f1c4cedcf2bbf
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2025-09-02 00:21:16 +0530

    Pattern questions started

---
+++ 1b. PATTERNs/1.py
@@ -0,0 +1,18 @@
+"""
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+
+
+def pattern(n):
+    for i in range(0, n):
+        for j in range(0, n):
+            print("* ", end="")
+        print()
+
+
+n = int(input("-> "))
+pattern(n)
