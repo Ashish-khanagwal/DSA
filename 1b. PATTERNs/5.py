80078186eed88055ce5ca82567f8c6e56cca6259
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2025-09-02 03:29:42 +0530

    doing pattern questions

---
+++ 1b. PATTERNs/5.py
@@ -0,0 +1,18 @@
+"""
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+
+
+def pattern(n):
+    for i in range(0, n):
+        for j in range(1, n - i + 1):
+            print("* ", end="")
+        print()
+
+
+n = int(input("-> "))
+pattern(n)
---
+++ 1b. PATTERNs/6.py
@@ -0,0 +1,18 @@
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
+
+
+def pattern(n):
+    for i in range(n, 0, -1):
+        for j in range(1, i + 1):
+            print(f"{j} ", end="")
+        print()
+
+
+n = int(input("-> "))
+pattern(n)
