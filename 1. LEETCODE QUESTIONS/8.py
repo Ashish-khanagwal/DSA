7ed3a3819fee967e33abd5a7069425e6d9a0fcc6
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2025-09-06 19:53:24 +0530

    Knuth Morris Pratt Algorithm done

---
+++ 1. LEETCODE QUESTIONS/8.py
@@ -0,0 +1,60 @@
+"""
+PROBLEM 28 -> Find the Index of the First Occurrence in a String.
+
+PROBLEM LINK -> https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+
+
+def strStr(haystack: str, needle: str) -> int:
+    if needle == "":
+        return 0
+
+    # LPS (LONGEST PREFIX SUFIX) CREATION
+
+    lps = [0] * len(needle)
+    prevLPS, i = 0, 1
+    while i < len(needle):
+        if needle[i] == needle[prevLPS]:
+            lps[i] = prevLPS + 1
+            prevLPS += 1
+            i += 1
+        elif prevLPS == 0:
+            lps[i] = 0
+            i += 1
+        else:
+            prevLPS = lps[prevLPS - 1]
+
+    # KMP LOGIC
+    i = j = 0
+    while i < len(haystack):
+        if haystack[i] == needle[j]:
+            i, j = i + 1, j + 1
+        else:
+            if j == 0:
+                i += 1
+            else:
+                j = lps[j - 1]
+        if j == len(needle):
+            return i - len(needle)
+    return -1
+
+
+if __name__ == "__main__":
+    print(strStr("sadbusted", "sad"))
+    print(strStr("leetcode", "leeto"))
