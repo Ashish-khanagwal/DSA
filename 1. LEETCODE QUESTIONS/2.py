03810dbbd0f9181a6c6aa0befb071b3005399b5c
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2025-09-26 20:28:14 +0530

    did Two sum using hashmap

--- 1. LEETCODE QUESTIONS/2.py
+++ 1. LEETCODE QUESTIONS/2.py
@@ -13,14 +13,32 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """
 
 
-def twoSum(nums: [int], target: int) -> [int]:
+'''
+This solution does gives the answer but it is O(n^2) time complexity
+
+def twoSum(nums: list[int], target: int) -> list[int]:
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 num = [i, j]
-                print(num)
+                return [i, j]
 
 
-nums = [2, 0, 2, 4]
-target = 4
-twoSum(nums, target)
+print(twoSum([2,7,11,15], 9))
+print(twoSum([3, 2, 4], 6))
+print(twoSum([3, 3], 6))
+'''
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    hashmap = {}
+    for i, num in enumerate(nums):
+        value = target - num
+        if value in hashmap:
+            return [hashmap[value], i]
+        hashmap[num] = i
+    return []
+
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 2, 4], 6))
+print(twoSum([3, 3], 6))
---
+++ 1. LEETCODE QUESTIONS/53.py
@@ -0,0 +1,29 @@
+'''
+PROBLEM 383 -> Ransom Note
+
+PROBLEM LINK -> https://leetcode.com/problems/ransom-note/
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by 
+using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+'''
+
+def canConstruct(ransomeNote: str, magazine: str) -> bool:
+  l, r = 0, len(ransomeNote)
+
+print(canConstruct("a", "b"))
+print(canConstruct("aa", "ab"))
+print(canConstruct("aa", "aab"))
